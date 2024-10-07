4dc1c1883d7b8d35daf92a927fe7cdaa4bf7974b
benjamin <benschac@gmail.com>
2024-10-07 10:13:36 -0400

    add mint screen on native and web

--- routes.d.ts
+++ routes.d.ts
@@ -2,18 +2,9 @@ import type { OneRouter } from 'one'
 
 declare module 'one' {
   export namespace OneRouter {
-    export interface __routes<T extends string = string>
-      extends Record<string, unknown> {
-      StaticRoutes:
-        | `/`
-        | `/(feed)`
-        | `/(feed)/`
-        | `/_sitemap`
-        | `/notifications`
-        | `/profile`
-      DynamicRoutes:
-        | `/(feed)/post/${OneRouter.SingleRoutePart<T>}`
-        | `/post/${OneRouter.SingleRoutePart<T>}`
+    export interface __routes<T extends string = string> extends Record<string, unknown> {
+      StaticRoutes: `/` | `/(feed)` | `/(feed)/` | `/_sitemap` | `/mint` | `/notifications` | `/profile`
+      DynamicRoutes: `/(feed)/post/${OneRouter.SingleRoutePart<T>}` | `/post/${OneRouter.SingleRoutePart<T>}`
       DynamicRouteTemplate: `/(feed)/post/[id]` | `/post/[id]`
       IsTyped: true
     }
